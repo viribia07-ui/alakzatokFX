04bc954d4cd3a50dd771904f7204580970ac0553
Bianka Virágh <viribia07@gmail.com>
2026-09-11 13:29:17 +0200

    Changed FX

--- src/main/java/com/kando/alakzatokfx/HelloController.java
+++ src/main/java/com/kando/alakzatokfx/HelloController.java
@@ -4,11 +4,12 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    //@FXML
+    //private Label welcomeText;
 
-    @FXML
+    /*@FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    }*/
+
 }
