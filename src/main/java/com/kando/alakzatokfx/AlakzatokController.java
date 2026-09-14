97f648bf89fc6a8572ccb92a3e3587fc1434abc0
Bianka Viragh <viribia07@gmail.com>
2026-09-14 10:56:03 +0200

    Changed the code

--- src/main/java/com/kando/alakzatokfx/AlakzatokController.java
+++ src/main/java/com/kando/alakzatokfx/AlakzatokController.java
@@ -4,18 +4,42 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
 public class AlakzatokController {
-    //@FXML
-    //private Label welcomeText;
 
     /*@FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }*/
-    @FXML
     public Button buttonAdd, buttonDelete, buttonSave;
     @FXML
     public RadioButton radioRed, radioGreen, radioBlue, radioSquare, radioCircle, radioTriangle;
     @FXML
     public ListView<String> savedList;
+
+    public void onRadioSetRed(){
+
+    }
+    public void onRadioSetGreen(){
+
+    }
+    public void onRadioSetBlue(){
+
+    }
+
+    public void onRadioSetSquare(){
+
+    }
+    public void onRadioSetCircle(){
+
+    }
+    public void onRadioSetTriangle(){
+
+    }
+
+    public void onButtonAdd(){
+
+    }
+    public void onButtonDelete(){
+
+    }
+    public void onButtonSave(){
+
+    }*/
 
 }
