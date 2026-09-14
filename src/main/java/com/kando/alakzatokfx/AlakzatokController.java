4cfffe12129a0bc5c8b708fdb2a40b8be46656ef
Bianka Viragh <viribia07@gmail.com>
2026-09-14 09:46:05 +0200

    Imported files from main branch Changed filenames

---
+++ src/main/java/com/kando/alakzatokfx/AlakzatokController.java
@@ -0,0 +1,12 @@
+package com.kando.alakzatokfx;
+
+public class AlakzatokController {
+    //@FXML
+    //private Label welcomeText;
+
+    /*@FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }*/
+
+}
