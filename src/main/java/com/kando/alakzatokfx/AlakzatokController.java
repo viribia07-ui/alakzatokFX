c8c9a4ca2b14739ed993ed59ce9c9b438dbedb11
DarkColdFairy <157730227+DarkColdFairy@users.noreply.github.com>
2026-09-20 19:15:36 +0200

    Solved problem between controller and fxml Radio buttons of colors now can change the bg color

--- src/main/java/com/kando/alakzatokfx/AlakzatokController.java
+++ src/main/java/com/kando/alakzatokfx/AlakzatokController.java
@@ -2,35 +2,49 @@ package com.kando.alakzatokfx;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+import java.util.Objects;
 
 public class AlakzatokController {
 
-    /*@FXML
+    @FXML
     public Button buttonAdd, buttonDelete, buttonSave;
     @FXML
     public RadioButton radioRed, radioGreen, radioBlue, radioSquare, radioCircle, radioTriangle;
     @FXML
     public ListView<String> savedList;
+    @FXML
+    public AnchorPane changeColor;
+    @FXML
+    public ImageView geometryImg;
 
     public void onRadioSetRed(){
-
+        changeColor.setStyle("-fx-background-color: red; -fx-border-color: black");
     }
     public void onRadioSetGreen(){
-
+        changeColor.setStyle("-fx-background-color: green; -fx-border-color: black");
     }
     public void onRadioSetBlue(){
-
+        changeColor.setStyle("-fx-background-color: blue; -fx-border-color: black");
     }
 
-    public void onRadioSetSquare(){
+    //@FXML
+    //public void onRadioSetSquare(){
+    //    geometryImg.setImage(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/kando/alakzatokfx/icons/negyzet.png"))));
+    //}
 
-    }
-    public void onRadioSetCircle(){
+    //@FXML
+    //public void onRadioSetCircle(){
+    //    geometryImg.setImage(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/kando/alakzatokfx/icons/kor.png"))));
+    //}
 
-    }
-    public void onRadioSetTriangle(){
-
-    }
+    //@FXML
+    //public void onRadioSetTriangle(){
+    //    geometryImg.setImage(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/kando/alakzatokfx/icons/haromszog.png"))));
+    //}
 
     public void onButtonAdd(){
 
@@ -40,6 +54,6 @@ public class AlakzatokController {
     }
     public void onButtonSave(){
 
-    }*/
+    }
 
 }
